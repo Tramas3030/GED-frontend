2ee0454191d84b07096d7e27cfcdee4d3b3b43ca
Matheus Raphael <matheusraphael0902@gmail.com>
2024-12-22 17:11:49 -0300

    created project structure

---
+++ src/styles/global.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing_: antialiased;
+    -moz-osx-font-smoothing_: grayscale;
+  }
+
+  :root {
+	  font-size: 62.5%;
+  }
+
+  body {
+    background-color: ${props => props.theme.black};
+    color: ${props => props.theme.white};
+
+    font-family: Roboto, sans-serif;
+    font-size: 1.6rem;
+  }
+`;
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,10 @@
+export const defaultTheme = {
+  white: '#fff',
+  black: '#141c33',
+  grey: '#eff5fa',
+  
+  'blue-600': '#2f456f',
+  'blue-500': '#5374ac',
+  'blue-300': '#8bafd0',
+
+} as const;
