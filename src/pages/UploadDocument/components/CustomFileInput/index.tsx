67989e5853cb81a94887c6de908dcd0cece3498a
Matheus Raphael <matheusraphael0902@gmail.com>
2025-01-02 22:04:42 -0300

    Add react hook form to upload document page

--- src/pages/UploadDocument/components/CustomFileInput/index.tsx
+++ src/pages/UploadDocument/components/CustomFileInput/index.tsx
@@ -1,15 +1,20 @@
 import { UploadSimple } from "phosphor-react";
+import { forwardRef } from "react";
 import { FileInputContainer, HiddenFileInput, PlaceholderText, StyledLabel } from "./styles";
 
-export function CustomFileInput() {
-  return(
-    <FileInputContainer>
-      <StyledLabel htmlFor="fileInput">
-        <PlaceholderText>Choose file</PlaceholderText>
-        <UploadSimple size={24}/>
-      </StyledLabel>
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface CustomFileInputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-      <HiddenFileInput type="file" id="fileInput" required/>
-    </FileInputContainer>
-  )
-}
+export const CustomFileInput = forwardRef<HTMLInputElement, CustomFileInputProps>((props , ref) => {
+    return(
+      <FileInputContainer>
+        <StyledLabel htmlFor={props.id}>
+          <PlaceholderText>Choose file</PlaceholderText>
+          <UploadSimple size={24}/>
+        </StyledLabel>
+
+        <HiddenFileInput type="file" id="fileInput" required ref={ref} {...props}/>
+      </FileInputContainer>
+    )
+  }
+)
--- src/pages/UploadDocument/index.tsx
+++ src/pages/UploadDocument/index.tsx
@@ -1,17 +1,33 @@
+import { useForm } from "react-hook-form";
+
 import { MediumButton } from "../../components/MediumButton";
 import { CustomFileInput } from "./components/CustomFileInput";
 import { CategoryInputContainer, FormContainer } from "./styles";
 
+interface UploadFormInputs {
+  fileInput: FileList;
+  category: string;
+  tag: string;
+  description: string;
+};
+
 export function UploadDocument() {
+  const { register, handleSubmit, reset } = useForm<UploadFormInputs>();
+
+  function handleFormSubmit(data: UploadFormInputs) {
+    console.log(data);
+    reset();
+  }
+  
   return(
-    <FormContainer>
+    <FormContainer onSubmit={handleSubmit(handleFormSubmit)}>
       <div>
-        <CustomFileInput />
-        <CategoryInputContainer type="text" placeholder="Category" required/>
+        <CustomFileInput id="fileInput" {...register('fileInput')}/>
+        <CategoryInputContainer type="text" placeholder="Category" id="category" {...register('category')}/>
       </div>
 
-      <input type="text" placeholder="Add tags separeted by commas"/>
-      <input type="text" placeholder="Description"/>
+      <input type="text" placeholder="Add tags separeted by commas" id="tag" {...register('tag')}/>
+      <input type="text" placeholder="Description" id="description" {...register('description')}/>
 
       <MediumButton text="Upload document"/>
     </FormContainer>
